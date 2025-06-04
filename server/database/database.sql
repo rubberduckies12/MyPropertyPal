786a8b276dc56546d07e4d79eb962c4cf60949c0
gherkins05 <finlaysperkins@gmail.com>
2025-06-04 18:40:01 +0100

    Fixed many issues

--- server/database/database.sql
+++ server/database/database.sql
@@ -17,9 +17,19 @@ INSERT INTO property_status (status) VALUES
     ('Not Available')
 ON CONFLICT DO NOTHING;
 
+
+CREATE TABLE IF NOT EXISTS account (
+    id INT PRIMARY KEY CHECK (id > 0),
+    role INT NOT NULL REFERENCES role(id),
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    email_verified BOOLEAN NOT NULL DEFAULT FALSE
+    -- property INT NULL REFERENCES property(id)
+);
+
 CREATE TABLE IF NOT EXISTS property (
     id SERIAL PRIMARY KEY,
-    landlord INT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
+    landlord INT NOT NULL REFERENCES account(id) ON DELETE CASCADE,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(100) NOT NULL,
     city VARCHAR(50) NOT NULL,
@@ -29,11 +39,4 @@ CREATE TABLE IF NOT EXISTS property (
     property_status INT NOT NULL REFERENCES property_status(id) DEFAULT 4
 );
 
-CREATE TABLE IF NOT EXISTS user (
-    id INT PRIMARY KEY CHECK (id > 0),
-    role INT NOT NULL REFERENCES role(id),
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    property INT NULL REFERENCES property(id)
-);
+ALTER TABLE account ADD COLUMN IF NOT EXISTS property INT NULL REFERENCES property(id);
