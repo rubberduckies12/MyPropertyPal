78a15cd63b37b5e71cd702848f08bdc89350c770
gherkins05 <finlaysperkins@gmail.com>
2025-06-04 18:29:15 +0100

    Fixed More types

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,20 +1,11 @@
-CREATE DATABASE IF NOT EXISTS landlordproject;
+-- landlordproject;
 
 CREATE TABLE role IF NOT EXISTS (
     id INT PRIMARY KEY,
     name VARCHAR(15) NOT NULL UNIQUE
 );
 
-CREATE TABLE user IF NOT EXISTS (
-    id INT PRIMARY KEY CHECK (id > 0),
-    role INT NOT NULL REFERENCES role(id),
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    property INT NULL REFERENCES property(id)
-);
-
-CREATE TABLE property_status IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS property_status (
     id SERIAL PRIMARY KEY,
     status VARCHAR(20) NOT NULL UNIQUE
 );
@@ -23,9 +14,10 @@ INSERT INTO property_status (status) VALUES
     ('Available'),
     ('Occupied'),
     ('Under Maintenance'),
-    ('Not Available');
+    ('Not Available')
+ON CONFLICT DO NOTHING;
 
-CREATE TABLE property IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS property (
     id SERIAL PRIMARY KEY,
     landlord INT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
     name VARCHAR(50) NOT NULL,
@@ -35,4 +27,13 @@ CREATE TABLE property IF NOT EXISTS (
     postcode VARCHAR(10) NOT NULL,
     flat_number VARCHAR(10) NULL,
     property_status INT NOT NULL REFERENCES property_status(id) DEFAULT 4
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY CHECK (id > 0),
+    role INT NOT NULL REFERENCES role(id),
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
+    property INT NULL REFERENCES property(id)
 );
