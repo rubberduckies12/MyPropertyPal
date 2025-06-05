998668ebe2302199a40926fd120b71583171fa12
gherkins05 <finlaysperkins@gmail.com>
2025-06-05 19:37:33 +0100

    Modified routes

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,16 @@ import './index.css';
 
 // Path imports
 import Login from './login/login.jsx';
+import Register from './register/register.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} /> {/* Thought the default was the landing page? */}
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
