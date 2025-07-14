fefe874be2fc01718309cb3840aff5215abe41bf
rubberduckies12 <rowetommy699@gmail.com>
2025-07-14 14:30:49 +0100

    s

--- webpage/next.config.mjs
+++ webpage/next.config.mjs
@@ -1,9 +1,9 @@
 /* @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Remove or comment out for normal build
-  //assetPrefix: '/MyPropertyPal/', // Only needed for static export
-  // basePath: '/MyPropertyPal',     // Only needed for static export
+  output: 'export',
+  assetPrefix: '/MyPropertyPal',
+  basePath: '/MyPropertyPal',
 };
 
 export default nextConfig;
