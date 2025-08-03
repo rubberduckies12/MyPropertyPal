221788d929b171dd88874eeeb40ca73a87d2d3d9
rubberduckies12 <rowetommy699@gmail.com>
2025-08-03 19:36:33 +0100

    pixel

--- marketing_site/src/pages/_app.js
+++ marketing_site/src/pages/_app.js
@@ -35,6 +35,38 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
       <Analytics />
 
+      {/* Conditionally Load Meta Pixel Code */}
+      {cookiesAccepted && (
+        <>
+          {/* Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '741425801937993');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=741425801937993&ev=PageView&noscript=1"
+            />
+          </noscript>
+          {/* End Meta Pixel Code */}
+        </>
+      )}
+
       {/* Cookie Banner */}
       {cookiesAccepted === null && (
         <div className="fixed bottom-0 left-0 right-0 bg-blue-600 text-white py-4 px-6 shadow-lg z-50 flex justify-between items-center animate-slide-up">
