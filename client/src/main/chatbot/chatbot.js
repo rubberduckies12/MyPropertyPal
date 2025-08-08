a5257a14092f92b1b6e1c54d9299b00180fff935
rubberduckies12 <rowetommy699@gmail.com>
2025-08-08 12:10:23 +0100

    this

--- client/src/main/chatbot/chatbot.js
+++ client/src/main/chatbot/chatbot.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.mypropertypal.com/chatbot";
+const API_URL = "https://api.mypropertypal.com/chat";
 
 export async function fetchChatbotReply(chatHistory) {
   const response = await fetch(API_URL, {
